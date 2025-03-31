69266ede984b7e2f8880082e2ac689502b5bdf99
B-ia1119205 <beatriz.alves20@escola.pr.gov.br>
2025-03-31 08:05:37 -0300

    Create sketch.js
    Projeto do P5.js

---
+++ sketch.js
@@ -0,0 +1,34 @@
+Criei o arquivo sketch.js com o código do projeto e o link.
+  
+-Codigos:
+
+  let x;
+let y;
+
+function setup() {
+  createCanvas(400, 400);
+  x = random(400);
+  x = int(x);
+  y = random(400);
+  y = int(y);
+}
+
+function draw() {
+  background(220);
+  x = x + random(-5,5);
+  y = y + random(-5,5);
+  x = constrain(x,0,400);
+  y = constrain(y,0,400);
+  let distancia;
+  distancia = dist(mouseX, mouseY, x, y);
+  circle(mouseX, mouseY, distancia);
+  //circle(x, y, 10);
+
+  if (distancia < 3) {
+    text("Encontrei!", 200, 200);
+    noLoop();
+  }
+}
+
+-Link:
+https://editor.p5js.org/beatriz.alves20/full/9ATQmkEv1
